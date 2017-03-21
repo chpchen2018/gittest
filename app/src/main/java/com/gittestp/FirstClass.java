71949b372b77c201aac8e3702b9b33119aec0329
chenchengpei <chenchengpei@cheyipai.com>
2017-03-21 18:27:50 +0800

    First Edit with Branch 1.0.1

---
+++ app/src/main/java/com/gittestp/FirstClass.java
@@ -0,0 +1,16 @@
+package com.gittestp;
+
+/**
+ * Created by Administrator on 2017/3/21.
+ */
+
+public class FirstClass {
+    /**
+     * 说明33333333
+     */
+
+
+
+
+
+}
---
+++ app/src/main/java/com/gittestp/SecondClass.java
@@ -0,0 +1,11 @@
+package com.gittestp;
+
+/**
+ * Created by Administrator on 2017/3/21.
+ */
+
+public class SecondClass {
+    /**
+     * 说明222222
+     */
+}
